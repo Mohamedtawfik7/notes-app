d153161edefe5f70eb26bc52605c9a1c721ee965
Mohamedtawfik7 <mohamedtefz@gmail.com>
2026-05-24 19:12:37 +0300

    create customize appbar using customized widget {NotesView , NotesViewBody , CustomAppBar , CustomSearshIcon}

---
+++ lib/views/notes_view_body.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/views/custom_appbar.dart';
+
+class NotesViewBody extends StatelessWidget {
+  const NotesViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(children: [SizedBox(height: 30), CustomAppBar()]),
+      ),
+    );
+  }
+}
