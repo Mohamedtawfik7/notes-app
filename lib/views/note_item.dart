3caeeb29b6bc8fd69ade79d780b1a09b65d73725
Mohamedtawfik7 <mohamedtefz@gmail.com>
2026-06-03 15:32:10 +0300

    creat notes list

---
+++ lib/views/note_item.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(top: 24, bottom: 24, left: 14),
+
+      decoration: BoxDecoration(
+        color: Color(0xffFFCC80),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: Text(
+              'Flutter Tips',
+              style: TextStyle(color: Colors.black, fontSize: 28),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.only(top: 16),
+              child: Text(
+                'Choose your custom masion , Tawfik',
+                style: TextStyle(
+                  color: Colors.black.withOpacity(.6),
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: FaIcon(
+                FontAwesomeIcons.trash,
+                color: Colors.black,
+                size: 25,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 16),
+            child: Text(
+              'June 2,2026',
+              style: TextStyle(color: Colors.black.withOpacity(.5)),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/views/notes_view_body.dart
+++ lib/views/notes_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/views/custom_appbar.dart';
+import 'package:notes_app/views/notes_list_view.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
@@ -9,7 +10,13 @@ class NotesViewBody extends StatelessWidget {
     return Container(
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(children: [SizedBox(height: 30), CustomAppBar()]),
+        child: Column(
+          children: [
+            SizedBox(height: 30),
+            CustomAppBar(),
+            Expanded(child: NotesListView()),
+          ],
+        ),
       ),
     );
   }
