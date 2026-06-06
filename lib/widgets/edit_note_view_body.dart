20da2223940549745d9b366ef7e638c51a4d7bfd
Mohamedtawfik7 <mohamedtefz@gmail.com>
2026-06-06 05:37:57 +0300

    Finish Edit Notes View UI

--- lib/widgets/edit_note_view_body.dart
+++ lib/widgets/edit_note_view_body.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/custom_appbar.dart';
+import 'package:notes_app/views/custom_text_field.dart';
 
 class EditNoteViewBody extends StatelessWidget {
   const EditNoteViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 22),
+      child: Column(
+        children: const [
+          SizedBox(
+            height: 100,
+            child: CustomAppBar(title: 'Edit Notes', icon: Icons.check),
+          ),
+          SizedBox(height: 50),
+          CustomTextField(hint: 'Title'),
+          SizedBox(height: 50),
+          CustomTextField(hint: 'Content', maxLines: 7),
+        ],
+      ),
+    );
   }
 }
