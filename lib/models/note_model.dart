aafea2a0581d53437bec9671cd3bb1c84a3a71ad
Mohamedtawfik7 <mohamedtefz@gmail.com>
2026-06-06 06:58:54 +0300

    add hive and init

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,23 +1,20 @@
+import 'package:hive/hive.dart';
+part 'note_model.g.dart';
 
-import 'package:hive_flutter/adapters.dart';
-part 'note_model.g.dart;
 @HiveType(typeId: 0)
 class NoteModel extends HiveObject {
   @HiveField(0)
   final String title;
-    @HiveField(1)
-
-  final String subtitle;
-    @HiveField(2)
-
+  @HiveField(1)
+  final String subTitle;
+  @HiveField(2)
   final String date;
-    @HiveField(3)
-
+  @HiveField(3)
   final int color;
 
   NoteModel({
     required this.title,
-    required this.subtitle,
+    required this.subTitle,
     required this.date,
     required this.color,
   });
