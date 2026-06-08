6f95b3e05634c636fe900de0de9475cf5df3b122
Mohamedtawfik7 <mohamedtefz@gmail.com>
2026-06-09 02:21:29 +0300

    refactor(cubit): integrate hive storage in addNote method
    Update the addNote method to interact with the Hive database.
    The implementation now manages state transitions through loading,
    success, and failure states during the asynchronous write operation.

--- lib/cubit/add_notes_cubit.dart
+++ lib/cubit/add_notes_cubit.dart
@@ -1,11 +1,22 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:meta/meta.dart';
 import 'package:notes_app/models/note_model.dart';
+import 'package:notes_app/views/constants.dart';
 
 part 'add_notes_state.dart';
 
 class AddNotesCubit extends Cubit<AddNotesState> {
   AddNotesCubit() : super(AddNotesInitial());
 
-  addNote(NoteModel note) {}
+  addNote(NoteModel note) async {
+    emit(AddNotesLoading());
+    try {
+      var notesBox = Hive.box<NoteModel>(kNotesBox);
+      emit(AddNotesSuccess());
+      await notesBox.add(note);
+    } catch (e) {
+      AddNotesFailure(e.toString());
+    }
+  }
 }
